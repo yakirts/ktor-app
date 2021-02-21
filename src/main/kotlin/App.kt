1faa2257d513008add7ffe555b5bddd2f2437a4d
yakirt <yakirt@voyagerlabs.co>
2021-02-21 21:02:00 +0200

    remove const file path

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -3,7 +3,7 @@ import com.runner.IguazioAssignmentRunner
 
 suspend fun main() {
     IguazioAssignmentRunner(
-            inputFilePath = "/home/yakirt/Downloads/dha-20210216T081141Z-001/Dina- home assignment/370098-lines.txt",
+            inputFilePath = "input path here",
             outputFilePath = "./output.txt",
             endPoint1 = "http://localhost:8081/",
             endPoint2 = "http://localhost:8082/"
