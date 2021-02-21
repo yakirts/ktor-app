d99be9273a51e37d4ae31a7ace0c24b0d7f1fb46
yakirt <yakirt@voyagerlabs.co>
2021-02-21 10:32:13 +0200

    extract to methods for simplicity

--- src/main/kotlin/com/runner/SimpleHttpRunner.kt
+++ src/main/kotlin/com/runner/SimpleHttpRunner.kt
@@ -7,19 +7,16 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.features.json.*
 import io.ktor.client.request.*
 import io.ktor.http.*
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.*
 import java.io.Closeable
 import java.io.File
 import java.io.FileNotFoundException
 
 class SimpleHttpRunner(
-    inputFilePath: String,
-    outputFilePath: String,
-    private val endPoint1: String,
-    private val endPoint2: String
+        inputFilePath: String,
+        outputFilePath: String,
+        private val endPoint1: String,
+        private val endPoint2: String
 ) : Closeable {
 
     private val inFile = File(inputFilePath)
@@ -54,45 +51,54 @@ class SimpleHttpRunner(
         inputMap.entries.chunked(chunkSize).forEach { chunk ->
             println("Sending $chunkSize AsyncRequests...")
 
-            chunk.forEach { entry ->
-                val offset = entry.key
-                val dataParam = entry.value
-                //async requests here
-                val dd1 = BiDataHolder.DeferredData(
-                    offset = offset,
-                    deferred = async(Dispatchers.IO) { sendRequest(httpClient, endPoint1, dataParam) }
-                )
-
-                val dd2 = BiDataHolder.DeferredData(
-                    offset = offset,
-                    deferred = async(Dispatchers.IO) { sendRequest(httpClient, endPoint2, dataParam) }
-                )
-                queue.addLast(BiDataHolder(dd1,dd2))
-            }
+            chunk.forEach { entry -> sendAsyncRequest(entry, queue) }
 
             println("Finish sending $chunkSize AsyncRequests...")
 
             println("Start processing response and write to disk...")
             while (qIter.hasNext()) {
-                val biDataHolder = qIter.next()
-                val response1Text = biDataHolder.dh1.deferred.await()
-                val response2Text = biDataHolder.dh2.deferred.await()
-                outFile.appendText(
-                    "${biDataHolder.dh1.offset} : $response1Text , $response2Text = ${
-                        stringMatcher.isMatch(
-                            response1Text,
-                            response2Text
-                        )
-                    }\n"
-                )
-                println("${biDataHolder.dh1.offset} : $response1Text , $response2Text = ${response1Text == response2Text}")
-                qIter.remove()
+                handleResponses(qIter)
             }
             println("Done processing response and write to disk")
         }
 
         val endTime = System.currentTimeMillis() - startTime
         println("Total time sending ${inputMap.size} request , and writing to disk [took ${endTime}ms]")
+    }
+
+    private suspend fun handleResponses(qIter: MutableIterator<BiDataHolder>) {
+        val biDataHolder = qIter.next()
+        val response1Text = biDataHolder.dh1.deferred.await()
+        val response2Text = biDataHolder.dh2.deferred.await()
+        outFile.appendText(
+                "${biDataHolder.dh1.offset} : $response1Text , $response2Text = ${
+                    stringMatcher.isMatch(
+                            response1Text,
+                            response2Text
+                    )
+                }\n"
+        )
+        println("${biDataHolder.dh1.offset} : $response1Text , $response2Text = ${response1Text == response2Text}")
+        qIter.remove()
+    }
+
+    private fun CoroutineScope.sendAsyncRequest(
+            entry: Map.Entry<Int, String>,
+            queue: ArrayDeque<BiDataHolder>
+    ) {
+        val offset = entry.key
+        val dataParam = entry.value
+        //async requests here
+        val dd1 = BiDataHolder.DeferredData(
+                offset = offset,
+                deferred = async(Dispatchers.IO) { sendRequest(httpClient, endPoint1, dataParam) }
+        )
+
+        val dd2 = BiDataHolder.DeferredData(
+                offset = offset,
+                deferred = async(Dispatchers.IO) { sendRequest(httpClient, endPoint2, dataParam) }
+        )
+        queue.addLast(BiDataHolder(dd1, dd2))
     }
 
     override fun close() {
@@ -106,16 +112,16 @@ private suspend fun sendRequest(client: HttpClient, url: String, param: String):
     return client.post {
         url(url)
         contentType(ContentType.Application.Json)
-        body  = Content(seed = param)
+        body = Content(seed = param)
     }
 }
 
 data class BiDataHolder(val dh1: DeferredData, val dh2: DeferredData) {
     data class DeferredData(
-        val deferred: Deferred<String>,
-        val offset: Int
+            val deferred: Deferred<String>,
+            val offset: Int
     )
 }
 
 
-data class Content(val seed:String)
+data class Content(val seed: String)
