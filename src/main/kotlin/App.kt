4d3381be14b408883b43d03d9a9fadf2476f40ab
yakirt <yakirt@voyagerlabs.co>
2021-02-24 17:11:02 +0200

    small change

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -3,7 +3,7 @@ import com.runner.SimpleHttpClientRunner
 
 suspend fun main() {
     SimpleHttpClientRunner(
-            inputFilePath = "/home/yakirt/Downloads/dha-20210216T081141Z-001/Dina- home assignment/370098-lines.txt",
+            inputFilePath = "input here",
             outputFilePath = "./output.txt",
             endPoint1 = "http://localhost:8081/",
             endPoint2 = "http://localhost:8081/"
